2457bbe9066f5900d32c180fd47ca3290eb14e72
Tero Multisilta <tero.multisilta@gmail.com>
2018-09-14 23:20:54 +0300

    Initial commit

---
+++ src/main/java/fi/multisilta/rahapossu/RahapossuApplication.java
@@ -0,0 +1,12 @@
+package fi.multisilta.rahapossu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RahapossuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RahapossuApplication.class, args);
+	}
+}
